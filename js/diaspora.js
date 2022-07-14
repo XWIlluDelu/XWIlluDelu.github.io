5a6378b2e1e8a8f6ede450ff5bb1d1bdde4d8c89
XWIlluDelu <XWIlluDelu@outlook.com>
2022-07-14 22:27:48 +0800

    Site updated: 2022-07-14 22:27:46

--- js/diaspora.js
+++ js/diaspora.js
@@ -105,8 +105,6 @@ var Diaspora = {
                     comment.click();
                 }
             }, 0)
-            var math = document.getElementById("single")
-            MathJax.Hub.Queue(["Typeset", MathJax.Hub, math])
         })
     },
     preview: function() {
@@ -419,7 +417,7 @@ $(function() {
 					if($("#hitokoto").data('st') == true){
 						$.get("https://v1.hitokoto.cn/", function (data) {
 						var data = data;
-						var str =  data.hitokoto + " ——  By "		
+						var str =  data.hitokoto + " —— "		
 						var options = {
 						  strings: [ 
 							//str + "Who??^1000",
@@ -545,7 +543,7 @@ $(function() {
                 }else{
                   hash = $(e.target).attr('href')
                 }
-                to  = $(decodeURI(hash))
+                to  = $("a.headerlink[href='" + hash + "']")
                 $("html,body").animate({
                   scrollTop: to.offset().top - 50
                 }, 300);
